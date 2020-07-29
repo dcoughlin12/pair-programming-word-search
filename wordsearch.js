d820ffb5b668bc7ee60ed41be1402f7b812a0575
Devin Coughlin <devin_coughlin@hotmail.com>
2020-07-29 22:37:02 +0000

    completed pair program for word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,24 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = require('../../../d2/Matrix_Transposition_Pair_Program.js');
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (word.length < 1 || letters.length < 1) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  let newLetters = transpose(letters);
+  //console.log(newLetters);
+  const verticalJoin = newLetters.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
